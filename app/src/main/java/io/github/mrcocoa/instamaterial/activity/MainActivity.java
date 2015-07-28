7a6e242557073358374dfa96667661908aa2b590
MrCocoa <huminwei@vip.qq.com>
2015-07-28 17:05:24 +0800

    added intro animations for toolbar, fab and feed items

--- app/src/main/java/io/github/mrcocoa/instamaterial/activity/MainActivity.java
+++ app/src/main/java/io/github/mrcocoa/instamaterial/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package io.github.mrcocoa.instamaterial.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,22 +9,42 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.OvershootInterpolator;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import io.github.mrcocoa.instamaterial.R;
 import io.github.mrcocoa.instamaterial.adapter.FeedAdapter;
+import io.github.mrcocoa.instamaterial.utils.Utils;
 
 /**
  * Created by cocoa on 15/7/28.
  * Email:cocoahoo@gmail.com
  **/
 public class MainActivity extends ActionBarActivity {
+    private static final int ANIM_DURATION_TOOLBAR = 300;
+    private static final int ANIM_DELAY_TOOLBAR = 300;
+
+    private static final int ANIM_DURATION_LOGO = 400;
+    private static final int ANIM_DELAY_LOGO = 400;
+
+    private static final int ANIM_DURATION_FAB = 400;
+    private static final int ANIM_DELAY_FAB = 400;
+
+    private static final int ANIM_DURATION_MENUITEM = 300;
+    private static final int ANIM_DELAY_MENUITEM = 500;
+
     @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.ivLogo) ImageView ivLogo;
     @Bind(R.id.rvFeed) RecyclerView rvFeed;
+    @Bind(R.id.btnCreate) ImageButton btnCreate;
 
     private MenuItem inboxMenuItem;
     private FeedAdapter feedAdapter;
+
+    private boolean pendingIntroAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +54,10 @@ public class MainActivity extends ActionBarActivity {
 
         setupToolbar();
         setupFeed();
+
+        if (savedInstanceState == null){
+            pendingIntroAnimation = true;
+        }
     }
 
     private void setupToolbar() {
@@ -51,6 +77,50 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         inboxMenuItem = menu.findItem(R.id.action_inbox);
         inboxMenuItem.setActionView(R.layout.menu_item_view);
+        if (pendingIntroAnimation){
+            pendingIntroAnimation = false;
+            startIntroAnimation();
+        }
         return true;
+    }
+
+    private void startIntroAnimation() {
+        btnCreate.setTranslationY(2 * getResources().getDimensionPixelOffset(R.dimen.btn_fab_size));
+
+        int actionbarSize = Utils.dpToPx(56);
+        toolbar.setTranslationY(-actionbarSize);
+        ivLogo.setTranslationY(-actionbarSize);
+        inboxMenuItem.getActionView().setTranslationY(-actionbarSize);
+
+        toolbar.animate()
+                .translationY(0)
+                .setDuration(ANIM_DURATION_TOOLBAR)
+                .setStartDelay(ANIM_DELAY_TOOLBAR);
+
+        ivLogo.animate()
+                .translationY(0)
+                .setDuration(ANIM_DURATION_LOGO)
+                .setStartDelay(ANIM_DELAY_LOGO);
+
+        inboxMenuItem.getActionView().animate()
+                .translationY(0)
+                .setDuration(ANIM_DURATION_MENUITEM)
+                .setStartDelay(ANIM_DELAY_MENUITEM)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animator) {
+                        startContentAnimation();
+                    }
+                }).start();
+    }
+
+    private void startContentAnimation() {
+        btnCreate.animate()
+                .translationY(0)
+                .setInterpolator(new OvershootInterpolator(1.f))
+                .setStartDelay(ANIM_DELAY_FAB)
+                .setDuration(ANIM_DURATION_FAB)
+                .start();
+        feedAdapter.updateItems();
     }
 }
--- app/src/main/java/io/github/mrcocoa/instamaterial/adapter/FeedAdapter.java
+++ app/src/main/java/io/github/mrcocoa/instamaterial/adapter/FeedAdapter.java
@@ -5,11 +5,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import io.github.mrcocoa.instamaterial.R;
+import io.github.mrcocoa.instamaterial.utils.Utils;
 import io.github.mrcocoa.instamaterial.widget.SquaredImageView;
 
 /**
@@ -20,7 +22,8 @@ public class FeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private static final int ANIMATED_ITEMS_COUNT = 2;
 
     private Context mContext;
-    private int itemsCount = 10;
+    private int itemsCount = 0;
+    private int lastAnimatedPosition = -1;
 
     public FeedAdapter(Context context) {
         this.mContext = context;
@@ -34,6 +37,7 @@ public class FeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+        runEnterAnimation(viewHolder.itemView, position);
         CellFeedViewHolder holder = (CellFeedViewHolder) viewHolder;
         if (position % 2 == 0) {
             holder.ivFeedCenter.setImageResource(R.drawable.img_feed_center_1);
@@ -44,14 +48,32 @@ public class FeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         }
     }
 
+    private void runEnterAnimation(View view, int position) {
+        if (position >= ANIMATED_ITEMS_COUNT - 1) {
+            return;
+        }
+
+        if (position > lastAnimatedPosition) {
+            lastAnimatedPosition = position;
+            view.setTranslationY(Utils.getScreenHeight(mContext));
+            view.animate()
+                    .translationY(0)
+                    .setInterpolator(new DecelerateInterpolator(3.f))
+                    .setDuration(700)
+                    .start();
+        }
+    }
+
     @Override
     public int getItemCount() {
         return itemsCount;
     }
 
     public static class CellFeedViewHolder extends RecyclerView.ViewHolder {
-        @Bind(R.id.ivFeedCenter) SquaredImageView ivFeedCenter;
-        @Bind(R.id.ivFeedBottom) ImageView ivFeedBottom;
+        @Bind(R.id.ivFeedCenter)
+        SquaredImageView ivFeedCenter;
+        @Bind(R.id.ivFeedBottom)
+        ImageView ivFeedBottom;
 
         public CellFeedViewHolder(View view) {
             super(view);
@@ -59,4 +81,8 @@ public class FeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         }
     }
 
+    public void updateItems() {
+        itemsCount = 30;
+        notifyDataSetChanged();
+    }
 }
